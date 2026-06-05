1b8943537b47b73ac81ca523889af8982f06c228
MusangQim <musangqim03@gmail.com>
2026-06-05 22:22:44 +0800

    Add: Introduction to OLED Screen, Adafruit library

---
+++ hello_oled/hello_oled.ino
@@ -0,0 +1,21 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3C
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() 
+{
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

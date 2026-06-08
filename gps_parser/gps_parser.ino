f843b10be1ddabe3247617882080712178e71c80
MusangQim <musangqim03@gmail.com>
2026-06-09 01:08:26 +0800

    Update: settle up function parseGPRMC and updateOLED and main loop function

--- gps_parser/gps_parser.ino
+++ gps_parser/gps_parser.ino
@@ -1,38 +1,91 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <HardwareSerial.h>
+#include <cstring>
 
+HardwareSerial gpsSerial(1);
+char gLatitutde[12] = "";
+char gLongitude[12] = "";
+char gNS = ' ';
+char gEW = ' ';
+char gStatus = 'V';
+char gpsBuffer[100];
+int buffIndex = 0;
 
-
-void setup() 
+void setup()
 {
   Serial.begin(9600);
   gpsSerial.begin(9600);
+  char testline[] = "$GPRMC,065733.00,A,0144.1500,N,10354.0200,E,0.0,0.0,080626,,,A*54";
+  parseGPRMC(tesline);
+  updateOLED();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
-
-void parseGPRMC(line)
+void loop() 
 {
-  char gpsBuffer[] = "$GPRMC,123519,A,3139.00,n,10168.69,E,   "
+  while (gpsSerial.available() > 0)
+  {
+    char c = gpsSerial.read();
+    if (c == '\n')
+    {
+      gpsBuffer[buffIndex] = '\0';
+      if (strncmp(gpsBuffer, "$GPRMC", 6) == 0)
+      {
+        parseGPRMC(gpsBuffer);
+        updateOLED();
+      }
+      buffIndex = 0;
+    }
+    else if (buffIndex < 99)
+    {
+      gpsBuffer[buffIndex++] = c;
+    }
+  }
+}
 
+void parseGPRMC(char line[])
+{
+  char copy[100];
+  strcpy(copy, line);
+  int fieldNum = 0;
+  char *token = strtok(copy, ",");
+
+  while (token != NULL)
+  {
+    if (fieldNum == 2)
+      gStatus = token[0];
+    if (fieldNum == 3)
+      strcpy(gLatitude, token);
+    if (fieldNum == 4)
+      gNS = token[0];
+    if (fieldNum == 5)
+      strcpy(gLongitude, token);
+    if (fieldNum == 6)
+      gEW = token[0];
+    fieldNum++;
+    token = strtok(NULL, ",");
+  }
 }
 
 void updateOLED()
 {
-  char status;
-
   display.clearDisplay();
-  if status == "A"
+  if (gStatus == 'A')
   {
-    display.setCursor(,);
-    display.println("LAT: ")
-    display.setCursor(,);
-    display.println("LON: ")
-    display.setCursor(,);
-    display.println("FIX: YES")
+    display.setCursor(15,20);
+    display.print("LAT: ");
+    display.println(gLatitude);
+    display.setCursor(15,35);
+    display.print("LON: ");
+    display.println(gLongitude);
+    display.setCursor(15,50);
+    display.println("FIX: YES");
   }
+  else
+  {
+    display.setCursor(15, 20);
+    display.println("FIX: NO");
+  }
+  display.display();
 }
